e3510412f7602ea082ca34fc5b5d6802857152be
sasha chernikova <chernikova.sahsa@yandex.ru>
2025-02-26 20:07:23 +0300

    new

--- src/base_classes.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class BaseProduct(ABC):
-    @classmethod
-    @abstractmethod
-    def new_product(cls, *args, **kwargs):
-        pass
--- src/my_exceptions.py
+++
@@ -1,3 +0,0 @@
-class ZeroQuantityProduct(Exception):
-    def __init__(self, message=None):
-        super().__init__(message)
